870f7614fbb9246f452045a2c0559a2fc07f8c81
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-10-09 18:38:16 +0530

    esk

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { IoMdArrowRoundBack } from 'react-icons/io';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 function App() {
   return (<>
-    <div className='row mt-5'>
-      <div className='col-6'> <div className="ps-2"><IoMdArrowRoundBack  size={30}/></div></div>
+    <div className='row mt-3'>
+      <div className='col-6'> <div className="ps-1"><IoMdArrowRoundBack  size={30}/></div></div>
       <div className='col-6'><BsThreeDotsVertical size={30} style={{float: "right"}}/></div>
     </div>  
     <div className='row'>
