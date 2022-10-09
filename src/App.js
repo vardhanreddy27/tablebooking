5f3eaed68fc68b342a76bf2f66f019bfe86e04aa
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-10-09 19:23:35 +0530

    esadk

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
     <div className='col-12'><h5 className="ps-2 text-secondary pt-2">Book a table</h5> </div>
     <div className='col-12'><h2 className="ps-2 ">Absolute Barbecue</h2> </div>
        </div>
+       <div className='row mt-4'>
+        <div className='col pt-5 text-center'><span className="circle"></span>
+</div>
+       </div>
     </>
   )
 }
