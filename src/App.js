b18829c085f71ea40d64b3987c190fec9329f798
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-10-09 18:08:55 +0530

    Message over here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { IoMdArrowRoundBack } from 'react-icons/io';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <div className='row mt-5'>
+      <div className='col-6'> <div className="ps-3"><IoMdArrowRoundBack  size={30}/></div></div>
+      <div className='col-6'><BsThreeDotsVertical size={30} style={{float: "right"}}/></div>
+    </div>  
+    <div className='row'>
+      <div className='col-6'>body </div>
+       </div>
+    </>
+  )
 }
 
-export default App;
+export default App
