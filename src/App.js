a9029e1c9503e29c1a2c389f8a5a26bf4623bb89
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-10-09 18:35:40 +0530

    es

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BsThreeDotsVertical } from 'react-icons/bs';
 function App() {
   return (<>
     <div className='row mt-5'>
-      <div className='col-6'> <div className="ps-3"><IoMdArrowRoundBack  size={30}/></div></div>
+      <div className='col-6'> <div className="ps-2"><IoMdArrowRoundBack  size={30}/></div></div>
       <div className='col-6'><BsThreeDotsVertical size={30} style={{float: "right"}}/></div>
     </div>  
     <div className='row'>
